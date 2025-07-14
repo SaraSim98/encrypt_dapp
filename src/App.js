edd0f12eefa2a800cabee3e944e5cf26bb2b0103
Sara Simone <s292808@studenti.polito.it>
2025-07-14 18:11:21 +0200

    Rename component to EncryptAndSign

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-/*import React, { useState } from "react";
-import EncryptWithMetaMask from "./components/EncryptWithMetaMask";
-
-function App() {
-  const [error, setError] = useState("");
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h2>Cifratura con MetaMask</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <EncryptWithMetaMask setError={setError} />
-    </div>
-  );
-}
-
-export default App;*/
-import React, { useState } from "react";
 import EncryptAndSignSeparate from "./components/EncryptAndSignSeparate";
 
 function App() {
